9548f0e1bca3848ba734c4952686a8024b05a0d4
冰水不冰 <icewaternotice@gmail.com>
2024-12-03 19:03:42 +0800

    Auto commit from Unity Builder.

--- Assets/Editor/VersionIncrementor.cs
+++ Assets/Editor/VersionIncrementor.cs
@@ -4,27 +4,25 @@ using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-
+using TMPro;
 
 [InitializeOnLoad]
 public class VersionIncrementor : IPreprocessBuildWithReport
 {
     public int callbackOrder => 0;
-
-    static VersionIncrementor()
-    {
-
-    }
-
     public void OnPreprocessBuild(BuildReport report)
     {
-        string versionParts = string.Join(".", VersionParts());
-        EditProjectSettings(versionParts);
-        CommitAndPushToGit(versionParts);
+        UpdateVersion();
+        CommitAndPushToGit(PlayerSettings.bundleVersion);
     }
 
-    private static string[] VersionParts()
+    public void OnPostprocessBuild(BuildReport report)
+    {
+
+    }
+
+
+    private static void UpdateVersion()
     {
         string[] versionParts = PlayerSettings.bundleVersion.Split('.');
         if (versionParts.Length == 3)
@@ -32,7 +30,8 @@ public class VersionIncrementor : IPreprocessBuildWithReport
             if (int.TryParse(versionParts[2], out int patchVersion))
             {
                 patchVersion++;
-                return new string[] { versionParts[0], versionParts[1], patchVersion.ToString() };
+                PlayerSettings.bundleVersion = $"{versionParts[0]}.{versionParts[1]}.{patchVersion}";
+                UnityEngine.Debug.Log($"Version updated to {PlayerSettings.bundleVersion}");
             }
             else
             {
@@ -43,19 +42,17 @@ public class VersionIncrementor : IPreprocessBuildWithReport
         {
             UnityEngine.Debug.LogError("Version format is not correct. It should be like 1.0.0");
         }
-        return null;
     }
 
     private static void CommitAndPushToGit(string versionParts)
     {
         RunGitCommand("git add .");
-        RunGitCommand("git add ProjectSettings/ProjectSettings.asset");
-        RunGitCommand("git commit -m \"Auto commit from Unity Builder.\"");
-        RunGitCommand("git tag -a v" + versionParts + " -m \"Auto tag from Unity Builder.\"");
+        RunGitCommand("git commit -m \"Auto commit from Unity Builder. \"");
+        RunGitCommand("git tag -a v" + versionParts + " -m \"Auto tag from Unity Builder. \"");
         RunGitCommand("git push origin main");
         RunGitCommand("git push origin v" + versionParts);
 
-        UnityEngine.Debug.Log("Git commit and push done.");
+        UnityEngine.Debug.Log("Git commit and push done");
     }
 
     private static void RunGitCommand(string command)
@@ -71,24 +68,5 @@ public class VersionIncrementor : IPreprocessBuildWithReport
             process.WaitForExit();
             UnityEngine.Debug.Log(process.StandardOutput.ReadToEnd());
         }
-    }
-
-    private static void EditProjectSettings(string versionParts)
-    {
-       // using system io to edit the project settings file
-        string filepath =  "E:/Documents/UnityProject/Internet Empire/ProjectSettings/ProjectSettings.asset";
-        string[] lines = File.ReadAllLines(filepath);
-
-        for (int i = 0; i < lines.Length; i++)
-        {
-            if (lines[i].Contains("bundleVersion:"))
-            {
-                lines[i] = "  bundleVersion: " + versionParts;
-                break;
-            }
-        }
-
-        File.WriteAllLines(filepath, lines);
-
     }
 }
