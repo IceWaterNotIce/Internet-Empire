372d464fc2f3af46735e437816b519e3860bcedb
冰水不冰 <icewaternotice@gmail.com>
2024-12-03 19:33:55 +0800

    Auto commit from Unity Builder.

--- Assets/Scripts/LobbySceneManager.cs
+++ Assets/Scripts/LobbySceneManager.cs
@@ -1,23 +1,19 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class LobbySceneManager : MonoBehaviour
 {
     private BundleLoader bundleLoader;
 
-    [SerializeField]
-    private Transform characterSpawnPt;
 
     // Start is called before the first frame update
     void Start()
     {
         bundleLoader = GameObject.Find("BundleLoader").GetComponent<BundleLoader>();
-
-        bundleLoader.GetPrefabFromBundles<GameObject>("model", "Character", UpdateCharacter);
     }
 
-    private void UpdateCharacter(GameObject prefab)
+    public void GoToCityStreet()
     {
-        GameObject.Instantiate(prefab, characterSpawnPt.position, characterSpawnPt.rotation, characterSpawnPt);
+        SceneManager.LoadScene("CityStreet");
     }
 }
 
