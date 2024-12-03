98e962f08ecb14d3727d9e7351be66a93b6ff3ea
冰水不冰 <icewaternotice@gmail.com>
2024-12-03 17:05:22 +0800

    Auto commit from Unity Builder.

--- Assets/Editor/VersionIncrementor.cs
+++ Assets/Editor/VersionIncrementor.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
+using System.Diagnostics;
+using System.IO;
 
 [InitializeOnLoad]
 public class VersionIncrementor : IPreprocessBuildWithReport
@@ -21,16 +23,43 @@ public class VersionIncrementor : IPreprocessBuildWithReport
             {
                 patchVersion++;
                 PlayerSettings.bundleVersion = $"{versionParts[0]}.{versionParts[1]}.{patchVersion}";
-                Debug.Log($"版本已更新为 {PlayerSettings.bundleVersion}");
+                CommitAndPushToGit(PlayerSettings.bundleVersion);
+                UnityEngine.Debug.Log($"Version updated to {PlayerSettings.bundleVersion}");
             }
             else
             {
-                Debug.LogError("解析补丁版本失败。");
+                UnityEngine.Debug.LogError("Patch version is not a number.");
             }
         }
         else
         {
-            Debug.LogError("版本格式不正确。预期格式为: x.x.x");
+            UnityEngine.Debug.LogError("Version format is not correct. It should be like 1.0.0");
+        }
+    }
+
+    private static void CommitAndPushToGit(string versionParts)
+    {
+        RunGitCommand("git add .");
+        RunGitCommand("git commit -m \"Auto commit from Unity Builder. \"");
+        RunGitCommand("git tag -a v" + versionParts + " -m \"Auto tag from Unity Builder. \"");
+        RunGitCommand("git push origin main");
+        RunGitCommand("git push origin v" + versionParts);
+
+        UnityEngine.Debug.Log("Git commit and push done");
+    }
+
+    private static void RunGitCommand(string command)
+    {
+        ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+        processStartInfo.WorkingDirectory = Application.dataPath;
+        processStartInfo.RedirectStandardOutput = true;
+        processStartInfo.UseShellExecute = false;
+        processStartInfo.CreateNoWindow = false;
+
+        using (Process process = Process.Start(processStartInfo))
+        {
+            process.WaitForExit();
+            UnityEngine.Debug.Log(process.StandardOutput.ReadToEnd());
         }
     }
 }
