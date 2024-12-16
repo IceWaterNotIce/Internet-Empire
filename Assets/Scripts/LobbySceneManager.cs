7063d97db394d9576716f55900540d5fd415a782
冰水不冰 <icewaternotice@gmail.com>
2024-12-16 15:50:24 +0800

    新增登入面板、用戶資料面板和已保存遊戲面板的功能；實作清除玩家偏好的方法，並根據用戶登入狀態切換面板顯示

--- Assets/Scripts/LobbySceneManager.cs
+++ Assets/Scripts/LobbySceneManager.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using Unity.Services.Authentication;
 
 public class LobbySceneManager : MonoBehaviour
 {
-
+    [SerializeField] private GameObject LoginPanel;
+    [SerializeField] private GameObject UserProfilePanel;
+    [SerializeField] private GameObject SavedGamePanel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,5 +28,73 @@ public class LobbySceneManager : MonoBehaviour
     public void ClearPlayerPrefs()
     {
         PlayerPrefs.DeleteAll();
+        Debug.Log("PlayerPrefs Cleared");
+        CloseSavedGamePanel();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void NewGame()
+    {
+        GoToCityStreet();
+    }
+    public void ContinueGame()
+    {
+        GoToCityStreet();
+    }
+
+    public void OpenSavedGamePanel()
+    {
+        SavedGamePanel.SetActive(true);
+    }
+
+    public void CloseSavedGamePanel()
+    {
+        SavedGamePanel.SetActive(false);
+    }
+
+    public void CloseLoginPanel()
+    {
+        LoginPanel.SetActive(false);
+    }
+
+    public void CloseUserProfilePanel()
+    {
+        UserProfilePanel.SetActive(false);
+    }
+
+    
+
+
+ 
+
+    public void user(){
+        if (AuthenticationService.Instance.IsSignedIn)
+        {
+            if(UserProfilePanel.activeSelf)
+            {
+                UserProfilePanel.SetActive(false);
+            }
+            else
+            {
+                UserProfilePanel.SetActive(true);
+                LoginPanel.SetActive(false);
+            }
+        }
+        else
+        {
+            if(LoginPanel.activeSelf)
+            {
+                LoginPanel.SetActive(false);
+            }
+            else
+            {
+                LoginPanel.SetActive(true);
+                UserProfilePanel.SetActive(false);
+            }
+        }
     }
 }
